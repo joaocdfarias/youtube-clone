f46fd75a07d6bc1734fa859a2757397b88691ff4
João Carlos <joacdfariass@gmail.com>
2021-03-15 15:15:52 -0300

    removed: videos object

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+//Components Import
+
 import {
   makeStyles,
   AppBar,
@@ -19,6 +22,12 @@ import {
   useTheme,
   Switch,
 } from "@material-ui/core";
+
+//Json Import
+
+import videos from "./videos";
+
+//Icon Import
 
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
@@ -96,73 +105,6 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 14,
   },
 }));
-
-const videos = [
-  {
-    id: 1,
-    title: "A GALÁXIA MAIS PRÓXIMA DA NOSSA VIA-LÁCTEA",
-    channel: "João Carlos",
-    views: "11 mi de visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb1.jpg",
-  },
-  {
-    id: 2,
-    title: "ESTE LUGAR TEM O PÔR-DO-SOL MAIS BONITO DO MUNDO!",
-    channel: "João Carlos",
-    views: "957 mil visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb2.jpg",
-  },
-  {
-    id: 3,
-    title: "A FLORESTA QUE INSPIROU O JOGO SLENDERMAN",
-    channel: "João Carlos",
-    views: "106 mil visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb3.jpg",
-  },
-  {
-    id: 4,
-    title: "ANIMAIS FANTÁSTICOS AVISTADOS E GRAVADOS",
-    channel: "João Carlos",
-    views: "5,6 mi de visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb4.jpg",
-  },
-  {
-    id: 5,
-    title: "COMO É A VISTA DENTRO DE UM BURACO NEGRO?",
-    channel: "João Carlos",
-    views: "2,2 mi de visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb5.jpg",
-  },
-  {
-    id: 6,
-    title: "O DESERTO QUE FOI GRAVADO A SÉRIE O MANDALORIANO",
-    channel: "João Carlos",
-    views: "233 mil visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb6.jpg",
-  },
-  {
-    id: 7,
-    title: "VEJA O PLANETA DESCONHECIDO ATÉ PELA NASA!",
-    channel: "João Carlos",
-    views: "118 mil visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb7.jpg",
-  },
-  {
-    id: 8,
-    title: "VEJA O CARRO MAIS RÁPIDO DO MUNDO EM AÇÃO!",
-    channel: "João Carlos",
-    views: "1,9 mi de visualizações",
-    date: "há 1 semana",
-    thumb: "/images/thumb8.jpg",
-  },
-];
 
 function Home({ darkMode, setDarkMode }) {
   const classes = useStyles();
---
+++ src/videos.js
@@ -0,0 +1,68 @@
+const videos = [
+  {
+    id: 1,
+    title: "A GALÁXIA MAIS PRÓXIMA DA NOSSA VIA-LÁCTEA",
+    channel: "João Carlos",
+    views: "11 mi de visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb1.jpg",
+  },
+  {
+    id: 2,
+    title: "ESTE LUGAR TEM O PÔR-DO-SOL MAIS BONITO DO MUNDO!",
+    channel: "João Carlos",
+    views: "957 mil visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb2.jpg",
+  },
+  {
+    id: 3,
+    title: "A FLORESTA QUE INSPIROU O JOGO SLENDERMAN",
+    channel: "João Carlos",
+    views: "106 mil visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb3.jpg",
+  },
+  {
+    id: 4,
+    title: "ANIMAIS FANTÁSTICOS AVISTADOS E GRAVADOS",
+    channel: "João Carlos",
+    views: "5,6 mi de visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb4.jpg",
+  },
+  {
+    id: 5,
+    title: "COMO É A VISTA DENTRO DE UM BURACO NEGRO?",
+    channel: "João Carlos",
+    views: "2,2 mi de visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb5.jpg",
+  },
+  {
+    id: 6,
+    title: "O DESERTO QUE FOI GRAVADO A SÉRIE O MANDALORIANO",
+    channel: "João Carlos",
+    views: "233 mil visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb6.jpg",
+  },
+  {
+    id: 7,
+    title: "VEJA O PLANETA DESCONHECIDO ATÉ PELA NASA!",
+    channel: "João Carlos",
+    views: "118 mil visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb7.jpg",
+  },
+  {
+    id: 8,
+    title: "VEJA O CARRO MAIS RÁPIDO DO MUNDO EM AÇÃO!",
+    channel: "João Carlos",
+    views: "1,9 mi de visualizações",
+    date: "há 1 semana",
+    thumb: "/images/thumb8.jpg",
+  },
+];
+
+export default videos;
